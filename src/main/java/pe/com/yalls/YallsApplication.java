9dfe998cbfcbb0037375394d8b14cb7326361333
Alisson Araceli Mamani <117932418+alisson-2004@users.noreply.github.com>
2022-11-16 15:55:33 -0500

    Subida Inicial
    Subida Inicial del Proyecto

---
+++ src/main/java/pe/com/yalls/YallsApplication.java
@@ -0,0 +1,13 @@
+package pe.com.yalls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YallsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YallsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/yalls/YallsApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.yalls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YallsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
